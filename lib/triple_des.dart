eeb6ef5693e4cebe0f51691b66a3ff3790d735a1
yalsayegh <yalsayegh@citglobal.com>
2020-11-23 21:32:18 +0200

    bug fix

--- lib/triple_des.dart
+++ lib/triple_des.dart
@@ -6,12 +6,14 @@ class TripleDes {
   static const MethodChannel _channel = const MethodChannel('triple_des');
 
   static Future<String> decrypt3Des({String message, String key}) async {
-    final String version = await _channel.invokeMethod('decrypt3Des', <String, String>{"key": key, "message": message});
+    final String version =
+        await _channel.invokeMethod('decrypt3Des', <String, dynamic>{"key": key, "message": message});
     return version;
   }
 
   static Future<String> encrypt3Des({String message, String key}) async {
-    final String version = await _channel.invokeMethod('encrypt3Des', <String, String>{"key": key, "message": message});
+    final String version =
+        await _channel.invokeMethod('encrypt3Des', <String, dynamic>{"key": key, "message": message});
     return version;
   }
 }
